65dd0963934deb655f5c35bf7fd4c89d74463ab4
Jack Kelly <jack@openclimatefix.org>
2023-11-14 14:50:27 +0000

    Python code moved to python_api.rs

--- python/light_speed_io/light_speed_io/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = ["lib", "ffi"]
-
-import os
-from .ffi import ffi
-
-lib = ffi.dlopen(os.path.join(os.path.dirname(__file__), 'native.so'))
-del os
--- python/light_speed_io/light_speed_io/ffi.py
+++
@@ -1,7 +0,0 @@
-# auto-generated file
-import _cffi_backend
-
-ffi = _cffi_backend.FFI('ffi',
-    _version = 0x2601,
-    _types = b'',
-)
