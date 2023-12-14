6d658e2a18dfd6ab83959c2bd8d317568cc7ce2d
Jack Kelly <jack@openclimatefix.org>
2023-12-14 18:24:14 +0000

    rust auto formatting

--- src/io_uring.rs
+++ src/io_uring.rs
@@ -1,13 +1,12 @@
-use io_uring::{opcode, types, IoUring};
 use io_uring::squeue::PushError;
+use io_uring::{opcode, types, IoUring};
+use libc;
 use std::os::unix::fs::OpenOptionsExt;
 use std::os::unix::io::AsRawFd;
 use std::{fs, io};
-use libc;
 
 fn io_uring() -> io::Result<()> {
-    let mut ring = IoUring::builder()
-        .build(8)?;
+    let mut ring = IoUring::builder().build(8)?;
 
     let mut buf = vec![0; 1024];
 
@@ -16,12 +15,7 @@ fn io_uring() -> io::Result<()> {
         .custom_flags(libc::O_DIRECT)
         .open("README.md")?;
 
-    submit_read(
-        &mut ring,
-        &fd,
-        buf.as_mut_ptr(),
-        buf.len() as _
-    ).expect("submission queue full");
+    submit_read(&mut ring, &fd, buf.as_mut_ptr(), buf.len() as _).expect("submission queue full");
 
     ring.submit_and_wait(1)?;
 
@@ -38,18 +32,13 @@ fn io_uring() -> io::Result<()> {
 
 /// Note that the developer needs to ensure
 /// that the entry pushed into submission queue is valid (e.g. fd, buffer).
-fn submit_read(ring: &mut IoUring, fd: &fs::File, buf: *mut u8, len: u32) -> Result<(), PushError>
-{
+fn submit_read(ring: &mut IoUring, fd: &fs::File, buf: *mut u8, len: u32) -> Result<(), PushError> {
     let read_e = opcode::Read::new(types::Fd(fd.as_raw_fd()), buf, len)
         .build()
         .user_data(0x42);
 
-    unsafe {
-        ring.submission()
-            .push(&read_e)
-    }
+    unsafe { ring.submission().push(&read_e) }
 }
-
 
 #[cfg(test)]
 mod tests {
