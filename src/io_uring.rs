ebe5e8729ead9caf88256b068c38d4a0c3915428
Jack Kelly <jack@openclimatefix.org>
2023-12-19 16:17:46 +0000

    Compiles and runs! Using Box::into_raw. But returns read error!

--- src/io_uring.rs
+++ src/io_uring.rs
@@ -1,45 +1,68 @@
 use anyhow::Result;
 use io_uring::squeue::PushError;
 use io_uring::{opcode, types, IoUring};
-use std::io::ErrorKind;
+use std::fs;
 use std::os::unix::fs::OpenOptionsExt;
 use std::os::unix::io::AsRawFd;
 use std::path::PathBuf;
 use std::sync::atomic::{AtomicU32, Ordering};
 use std::sync::Arc;
-use std::{fs, io, mem};
 
-fn submit_and_process(tasks: &[PathBuf]) -> Vec<Result<Vec<u8>>> {
+struct OperationDescriptor {
+    buf: Vec<u8>,
+    task_i: usize,
+    path: PathBuf,
+    cqe: Option<io_uring::cqueue::Entry>,
+}
+
+fn submit_and_process(tasks: &[PathBuf]) {
+    // TODO: Return type should be -> Vec<Result<Vec<u8>>>
     const CQ_RING_SIZE: u32 = 16;
     let mut ring = IoUring::builder().build(CQ_RING_SIZE).unwrap();
-    let mut results: Vec<Result<Vec<u8>>> = Vec::with_capacity(tasks.len());
     let n_tasks_in_flight = Arc::new(AtomicU32::new(0));
 
     // Send tasks to threadpool:
     let mut task_i = 0;
     rayon::scope(|s| {
-        while task_i < tasks.len() && n_tasks_in_flight.load(Ordering::SeqCst) > 0 {
-            // Keep io_uring submission queue topped up:
-            while n_tasks_in_flight.load(Ordering::SeqCst) < CQ_RING_SIZE {
+        while task_i < tasks.len() {
+            // || n_tasks_in_flight.load(Ordering::SeqCst) > 0
+            // Keep io_uring submission queue topped up. But don't overload io_uring!
+            while task_i < tasks.len() && n_tasks_in_flight.load(Ordering::SeqCst) < CQ_RING_SIZE {
                 let task = &tasks[task_i];
-                let mut buf = vec![0u8; 1024];
+                println!("task_i={}, path={:?}", task_i, task);
 
+                // TODO: Open file using io_uring. See issue #1
                 let fd = fs::OpenOptions::new()
                     .read(true)
-                    .custom_flags(libc::O_DIRECT)
+                    // .custom_flags(libc::O_DIRECT)
                     .open(task)
-                    .unwrap(); // TODO: Handle error!
+                    .unwrap();
+
+                // Save information (so the processing thread can access this information).
+                // `into_raw()` consumes the object (but doesn't de-allocated it), which is exactly
+                // what we want. We mustn't touch buffer until it re-emerges from the kernel.
+                // And we do want Rayon's worker thread (that processes the CQE) to decide whether
+                // to drop the buffer (after moving data elsewhere) or keep the buffer
+                // (if we're passing the buffer back to the user).
+                let mut op_descriptor = Box::new(OperationDescriptor {
+                    // TODO: Allocate the correct sized buffer for the task.
+                    //       Or don't allocate at all, if the user has already allocated.
+                    buf: vec![0u8; 1024],
+                    path: task.clone(),
+                    task_i,
+                    cqe: None,
+                });
 
                 submit_read(
                     &mut ring,
                     &fd,
-                    buf.as_mut_ptr(),
-                    buf.len() as _,
-                    task_i as u64,
+                    op_descriptor.buf.as_mut_ptr(),
+                    op_descriptor.buf.len() as _,
+                    Box::into_raw(op_descriptor) as u64,
                 )
                 .expect("submission queue full"); // TODO: Handle PushError:
 
-                results[task_i] = Ok(buf);
+                // Increment counters
                 task_i += 1;
                 n_tasks_in_flight.fetch_add(1, Ordering::SeqCst);
             }
@@ -49,41 +72,37 @@ fn submit_and_process(tasks: &[PathBuf]) -> Vec<Result<Vec<u8>>> {
             // Spawn tasks to the Rayon ThreadPool to process data:
             for cqe in ring.completion() {
                 // Prepare data for thread:
-                // Handle return value from read():
-                let return_value = cqe.result();
-                if return_value < 0 {
-                    // An error has occurred!
-                    results[cqe.user_data() as usize] = Err(anyhow::Error::new(io::Error::new(
-                        ErrorKind::Other,
-                        format!("io_uring reported an error. Error code: {}", return_value),
-                    )));
-                    n_tasks_in_flight.fetch_sub(1, Ordering::SeqCst);
-                } else {
-                    let buf = results[cqe.user_data() as usize].as_ref().unwrap();
-                    let buf_ptr = buf.as_ptr() as usize;
-                    let buf_len = buf.len();
-                    let buf_cap = buf.capacity();
-                    // let result = mem::replace(&mut results[cqe.user_data() as usize], Ok(vec![0; 0]));
-                    let n_tasks_in_flight_for_thread = n_tasks_in_flight.clone();
-                    s.spawn(move |_| {
-                        // TODO: Handle when the number of bytes read is less than the number of bytes requested
-                        // TODO: process(cqe);
-                        unsafe {
-                            let buf_for_thread = Vec::<u8>::from_raw_parts(buf_ptr as *mut u8, buf_len, buf_cap);
-                            do_something(buf_for_thread); // Temporary name!
-                        }
+                let mut op_descriptor =
+                    unsafe { Box::from_raw(cqe.user_data() as *mut OperationDescriptor) };
+                op_descriptor.cqe = Some(cqe);
+                let n_tasks_in_flight_for_thread = n_tasks_in_flight.clone();
 
-                        // Move n_tasks_in_flight_for_thread into this thread:
-                        n_tasks_in_flight_for_thread.fetch_sub(1, Ordering::SeqCst);
-                    });
-                }
+                // Spawn task to Rayon's ThreadPool:
+                s.spawn(move |_| {
+                    do_something(op_descriptor);
+                    n_tasks_in_flight_for_thread.fetch_sub(1, Ordering::SeqCst);
+                });
             }
         }
     });
-    results
+    // TODO: Figure out how to return vectors (or errors)!
+    assert!(n_tasks_in_flight.load(Ordering::SeqCst) == 0);
 }
 
-fn do_something(_ :Vec<u8>) {
+fn do_something(op_descriptor: Box<OperationDescriptor>) {
+    println!("Reading {:?}", op_descriptor.path);
+    let cqe = op_descriptor.cqe.unwrap();
+
+    // Handle return value from read():
+    if cqe.result() < 0 {
+        // An error has occurred!
+        println!("Error reading file!");
+        // return;
+    }
+    // TODO: Handle when the number of bytes read is less than the number of bytes requested
+
+    let buf = op_descriptor.buf;
+    println!("{:?}", buf);
 }
 
 /// Note that the developer needs to ensure
@@ -104,11 +123,13 @@ fn submit_read(
 
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
+
     use super::*;
 
     #[test]
     fn it_works() {
-        let result = io_uring();
-        assert!(result.is_ok(), "Error: {}", result.unwrap_err());
+        let tasks = [PathBuf::from_str("/home/jack/dev/rust/light-speed-io/README.md").unwrap()];
+        submit_and_process(&tasks);
     }
 }
