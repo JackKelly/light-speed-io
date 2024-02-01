82877f7a2ca7e98b0d1d72d2ce37bea4e3321109
Jack Kelly <jack@openclimatefix.org>
2024-02-01 09:34:16 +0000

    Fix bug whereby inner loop would've been a tight loop if there's no tasks in flight and no messages in the channel. #30

--- src/io_uring_local/thread.rs
+++ src/io_uring_local/thread.rs
@@ -1,21 +1,31 @@
 use io_uring::IoUring;
-use std::sync::mpsc::{Receiver, TryRecvError};
+use std::sync::mpsc::{Receiver, RecvError, TryRecvError};
 
 use crate::io_uring_local::prep_operation_for_io_uring::prepare_io_uring_entry;
 use crate::operation_future::SharedState;
 
 pub(crate) fn worker_thread_func(rx: Receiver<SharedState>) {
-    const CQ_RING_SIZE: u32 = 16; // TODO: This should be user-configurable.
+    const CQ_RING_SIZE: u32 = 16; // TODO: Enable the user to configure this.
     let mut ring = IoUring::new(CQ_RING_SIZE).unwrap();
     let mut n_tasks_in_flight_in_io_uring: u32 = 0;
 
     'outer: loop {
         // Keep io_uring's submission queue topped up:
-        'inner: while n_tasks_in_flight_in_io_uring < CQ_RING_SIZE {
-            let shared_state = match rx.try_recv() {
-                Ok(s) => s,
-                Err(TryRecvError::Empty) => break 'inner,
-                Err(TryRecvError::Disconnected) => break 'outer,
+        // TODO: Extract this inner loop into a separate function!
+        'inner: loop {
+            let shared_state = match n_tasks_in_flight_in_io_uring {
+                0 => match rx.recv() {
+                    // There are no tasks in flight in io_uring, so all that's
+                    // left to do is to wait for more tasks.
+                    Ok(s) => s,
+                    Err(RecvError) => break 'outer, // The caller hung up.
+                },
+                CQ_RING_SIZE.. => break 'inner, // The CQ is full!
+                _ => match rx.try_recv() {
+                    Ok(s) => s,
+                    Err(TryRecvError::Empty) => break 'inner,
+                    Err(TryRecvError::Disconnected) => break 'outer,
+                },
             };
 
             // Convert `Operation` to a `PreparedEntry`.
