69d1b79feaefd96aa47e6c88480c4bebeac01f4a
Jack Kelly <jack@openclimatefix.org>
2024-02-12 13:29:05 +0000

    implement benchmark for object_store::LocalFileSystem. LSIO is about 4x faster!

--- benches/io_uring_local.rs
+++ benches/io_uring_local.rs
@@ -1,6 +1,6 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use light_speed_io::object_store_adapter::ObjectStoreAdapter;
-use object_store::path::Path as ObjectStorePath;
+use object_store::{path::Path as ObjectStorePath, ObjectStore};
 use std::{process::Command, time::Duration};
 use tokio::runtime::Runtime;
 
@@ -8,21 +8,41 @@ const FILE_SIZE_BYTES: usize = 262_144;
 const DATA_PATH: &str = "/home/jack/temp/fio/";
 
 async fn load_files_with_io_uring_local(filenames: &Vec<ObjectStorePath>) {
-    let n = filenames.len();
-
     // Start reading async:
     let store = ObjectStoreAdapter::default();
-    let mut futures = Vec::with_capacity(n);
+    let mut futures = Vec::with_capacity(filenames.len());
     for filename in filenames {
         futures.push(store.get(filename));
     }
-
-    // Wait for everything to complete:
-    let mut results = Vec::with_capacity(n);
+    let mut results = Vec::with_capacity(futures.len());
     for f in futures {
         let b = f.await.expect("At least one Result was an Error");
         assert!(b.len() == FILE_SIZE_BYTES);
         results.push(b);
+    }
+}
+
+async fn load_files_with_local_file_system(filenames: &Vec<ObjectStorePath>) {
+    // Unfortunately, I can't find a better way to share code between `load_files_with_io_uring_local`
+    // and `load_files_with_local_file_system` because `ObjectStoreAdapter` doesn't yet `impl ObjectStore`.
+    // And `ObjectStoreAdapter::get` and `LocalFileSystem::get` return slightly different types.
+    // TODO: Reduce duplication if/when `ObjectStoreAdapter` implements `ObjectStore`.
+
+    // Start reading async:
+    let store = object_store::local::LocalFileSystem::default();
+    let mut futures = Vec::with_capacity(filenames.len());
+    for filename in filenames {
+        futures.push(store.get(filename));
+    }
+    let mut results = Vec::with_capacity(futures.len());
+    for f in futures {
+        let get_result = f.await.expect("At least one GetResult was an Error");
+        results.push(get_result);
+    }
+
+    for get_result in results {
+        let b = get_result.bytes().await.unwrap();
+        assert!(b.len() == FILE_SIZE_BYTES);
     }
 }
 
@@ -54,6 +74,21 @@ fn bench(c: &mut Criterion) {
             criterion::BatchSize::SmallInput,
         );
     });
+
+    // Run function:
+    group.bench_function("local_file_system", |b| {
+        // Insert a call to `to_async` to convert the bencher to async mode.
+        // The timing loops are the same as with the normal bencher.
+        b.to_async(Runtime::new().unwrap()).iter_batched(
+            || {
+                clear_page_cache();
+                &filenames
+            },
+            |filenames| async { load_files_with_local_file_system(filenames).await },
+            criterion::BatchSize::SmallInput,
+        );
+    });
+
     group.finish();
 }
 
