1e9ad44d3c63b12557cadea252b28fda93b1fdc0
Jack Kelly <jack@openclimatefix.org>
2024-02-14 10:43:39 +0000

    Remove relative filename from unittest. #57

--- src/object_store_adapter.rs
+++ src/object_store_adapter.rs
@@ -197,8 +197,6 @@ mod tests {
         let filenames = vec![
             Path::from("///home/jack/dev/rust/light-speed-io/README.md"),
             Path::from("///home/jack/dev/rust/light-speed-io/Cargo.toml"),
-            Path::from("README.md"),
-            //Path::from("Cargo.toml"),
         ];
         let store = ObjectStoreAdapter::default();
         let mut futures = Vec::new();
