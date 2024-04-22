7d39e23e9a1b9e293569a4b619f72641c3aee0eb
Jack Kelly <jack@openclimatefix.org>
2024-04-22 18:17:51 +0100

    Using valid_slice: Range<usize>. Closes #115

--- crates/lsio_aligned_buffer/src/lib.rs
+++ crates/lsio_aligned_buffer/src/lib.rs
@@ -1,31 +1,25 @@
-use core::{ops::RangeBounds, slice};
+use core::{ops::Range, slice};
 use std::alloc;
 
 /// A memory buffer allocated on the heap, where the start position and end position of the backing
 /// buffer are both aligned. This is useful for working with O_DIRECT file IO, where the filesystem
 /// will often expect the buffer to be aligned to the logical block size (typically 512 bytes).
 #[derive(Debug)]
-pub struct AlignedBuffer<R>
-where
-    R: RangeBounds<usize> + ExactSizeIterator,
-{
+pub struct AlignedBuffer {
     buf: *mut u8,
     /// The slice requested by the user.
-    valid_slice: R,
+    valid_slice: Range<usize>,
     /// `layout.size()` gives the number of bytes _actually_ allocated, which will be
     /// a multiple of `align`.
     layout: alloc::Layout,
 }
 
-unsafe impl<R> Send for AlignedBuffer<R> where R: RangeBounds<usize> + ExactSizeIterator {}
+unsafe impl Send for AlignedBuffer {}
 
-impl<R> AlignedBuffer<R>
-where
-    R: RangeBounds<usize> + ExactSizeIterator,
-{
+impl AlignedBuffer {
     /// Aligns the start and end of the buffer with `align`.
     /// 'align' must not be zero, and must be a power of two.
-    pub(crate) fn new(slice: R, align: usize) -> Self {
+    pub(crate) fn new(slice: Range<usize>, align: usize) -> Self {
         assert_ne!(slice.len(), 0);
         // Let's say the user requests a buffer of len 3 and offset 2; and align is 4:
         //            index:     0 1 2 3 4 5 6 7
@@ -33,10 +27,9 @@ where
         //        requested:         |---|
         // In this case, we need to allocate 8 bytes becuase we need to move the start
         // backwards to the first byte, and move the end forwards to the eighth byte.
-        let layout =
-            alloc::Layout::from_size_align(slice.len() + (slice.start_bound() % align), align)
-                .expect("failed to create Layout!")
-                .pad_to_align();
+        let layout = alloc::Layout::from_size_align(slice.len() + (slice.start % align), align)
+            .expect("failed to create Layout!")
+            .pad_to_align();
         let buf = unsafe { alloc::alloc(layout) };
         if buf.is_null() {
             alloc::handle_alloc_error(layout);
@@ -49,7 +42,7 @@ where
     }
 
     pub(crate) fn aligned_start_offset(&self) -> usize {
-        (self.valid_slice.start_bound() / self.layout.align()) * self.layout.align()
+        (self.valid_slice.start / self.layout.align()) * self.layout.align()
     }
 
     /// The length of the `valid_slice` requested by the user. The `valid_slice` is a view into the
@@ -70,18 +63,14 @@ where
     pub fn as_slice(&self) -> &[u8] {
         unsafe {
             slice::from_raw_parts(
-                self.buf
-                    .offset(self.valid_slice.start_bound().try_into().unwrap()),
+                self.buf.offset(self.valid_slice.start.try_into().unwrap()),
                 self.len(),
             )
         }
     }
 }
 
-impl<R> Drop for AlignedBuffer<R>
-where
-    R: RangeBounds<usize> + ExactSizeIterator,
-{
+impl Drop for AlignedBuffer {
     fn drop(&mut self) {
         unsafe { alloc::dealloc(self.buf, self.layout) };
     }
@@ -95,13 +84,13 @@ mod tests {
     fn test_write_and_read() {
         // Create a new buffer:
         const LEN: usize = 16;
-        let mut aligned_buf1 = AlignedBuffer::new(..LEN, 8);
-        let mut aligned_buf2 = AlignedBuffer::new(..LEN, 8);
+        let mut aligned_buf1 = AlignedBuffer::new(0..LEN, 8);
+        let mut aligned_buf2 = AlignedBuffer::new(0..LEN, 8);
 
         // Set the values of the buffer:
         {
-            let ptr1 = aligned_buf1.as_ptr();
-            let ptr2 = aligned_buf2.as_ptr();
+            let ptr1 = aligned_buf1.as_ptr_to_underlying_buf();
+            let ptr2 = aligned_buf2.as_ptr_to_underlying_buf();
             unsafe {
                 for i in 0..LEN {
                     *ptr1.offset(i as _) = i as u8;
