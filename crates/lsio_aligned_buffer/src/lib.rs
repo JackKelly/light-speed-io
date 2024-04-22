7680584314684af437d8ccc5a11519444032076e
Jack Kelly <jack@openclimatefix.org>
2024-04-22 19:31:06 +0100

    AlignedBuffer works. No splitting yet.

--- crates/lsio_aligned_buffer/src/lib.rs
+++ crates/lsio_aligned_buffer/src/lib.rs
@@ -1,20 +1,62 @@
 use core::{ops::Range, slice};
-use std::alloc;
+use std::{alloc, sync::Arc};
 
 /// A memory buffer allocated on the heap, where the start position and end position of the backing
 /// buffer are both aligned. This is useful for working with O_DIRECT file IO, where the filesystem
 /// will often expect the buffer to be aligned to the logical block size (typically 512 bytes).
 #[derive(Debug)]
 pub struct AlignedBuffer {
-    buf: *mut u8,
+    buf: Arc<InnerAlignedBuffer>,
     /// The slice requested by the user.
     valid_slice: Range<usize>,
+}
+
+#[derive(Debug)]
+struct InnerAlignedBuffer {
+    buf: *mut u8,
     /// `layout.size()` gives the number of bytes _actually_ allocated, which will be
     /// a multiple of `align`.
     layout: alloc::Layout,
 }
 
 unsafe impl Send for AlignedBuffer {}
+unsafe impl Sync for AlignedBuffer {}
+
+impl InnerAlignedBuffer {
+    fn new(len: usize, align: usize) -> Self {
+        assert_ne!(len, 0);
+        let layout = alloc::Layout::from_size_align(len, align)
+            .expect("failed to create Layout!")
+            .pad_to_align();
+        let buf = unsafe { alloc::alloc(layout) };
+        if buf.is_null() {
+            alloc::handle_alloc_error(layout);
+        }
+        Self { buf, layout }
+    }
+
+    const fn size(&self) -> usize {
+        self.layout.size()
+    }
+
+    const fn align(&self) -> usize {
+        self.layout.align()
+    }
+
+    fn as_mut_ptr(&self) -> *mut u8 {
+        self.buf
+    }
+
+    fn as_ptr(&self) -> *const u8 {
+        self.buf
+    }
+}
+
+impl Drop for InnerAlignedBuffer {
+    fn drop(&mut self) {
+        unsafe { alloc::dealloc(self.buf, self.layout) };
+    }
+}
 
 impl AlignedBuffer {
     /// Aligns the start and end of the buffer with `align`.
@@ -27,22 +69,14 @@ impl AlignedBuffer {
         //        requested:         |---|
         // In this case, we need to allocate 8 bytes becuase we need to move the start
         // backwards to the first byte, and move the end forwards to the eighth byte.
-        let layout = alloc::Layout::from_size_align(slice.len() + (slice.start % align), align)
-            .expect("failed to create Layout!")
-            .pad_to_align();
-        let buf = unsafe { alloc::alloc(layout) };
-        if buf.is_null() {
-            alloc::handle_alloc_error(layout);
-        }
+        let inner_buf = Arc::new(InnerAlignedBuffer::new(
+            slice.len() + (slice.start % align),
+            align,
+        ));
         Self {
-            buf,
+            buf: inner_buf,
             valid_slice: slice,
-            layout,
         }
-    }
-
-    pub(crate) fn aligned_start_offset(&self) -> usize {
-        (self.valid_slice.start / self.layout.align()) * self.layout.align()
     }
 
     /// The length of the `valid_slice` requested by the user. The `valid_slice` is a view into the
@@ -52,29 +86,27 @@ impl AlignedBuffer {
     }
 
     /// The length of the underlying buffer.
-    pub(crate) const fn capacity(&self) -> usize {
-        self.layout.size()
+    pub(crate) fn capacity(&self) -> usize {
+        self.buf.size()
     }
 
-    pub(crate) fn as_ptr_to_underlying_buf(&mut self) -> *mut u8 {
-        self.buf
+    pub(crate) fn as_mut_ptr_to_underlying_buf(&mut self) -> *mut u8 {
+        self.buf.as_mut_ptr()
     }
 
     pub fn as_slice(&self) -> &[u8] {
         unsafe {
             slice::from_raw_parts(
-                self.buf.offset(self.valid_slice.start.try_into().unwrap()),
+                self.buf
+                    .as_ptr()
+                    .offset(self.valid_slice.start.try_into().unwrap()),
                 self.len(),
             )
         }
     }
 }
 
-impl Drop for AlignedBuffer {
-    fn drop(&mut self) {
-        unsafe { alloc::dealloc(self.buf, self.layout) };
-    }
-}
+// TODO: Implement clone?
 
 #[cfg(test)]
 mod tests {
@@ -89,8 +121,8 @@ mod tests {
 
         // Set the values of the buffer:
         {
-            let ptr1 = aligned_buf1.as_ptr_to_underlying_buf();
-            let ptr2 = aligned_buf2.as_ptr_to_underlying_buf();
+            let ptr1 = aligned_buf1.as_mut_ptr_to_underlying_buf();
+            let ptr2 = aligned_buf2.as_mut_ptr_to_underlying_buf();
             unsafe {
                 for i in 0..LEN {
                     *ptr1.offset(i as _) = i as u8;
