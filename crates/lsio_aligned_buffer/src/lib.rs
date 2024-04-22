569031e6f36c01dd8164d263c52debb3fffaebe7
Jack Kelly <jack@openclimatefix.org>
2024-04-22 18:12:31 +0100

    Made a start on using RangeBounds for the valid_slice, but it's a little painful and probably not necessary! So, in the next commit, I'll revert to using Range<usize>

--- crates/lsio_aligned_buffer/src/lib.rs
+++ crates/lsio_aligned_buffer/src/lib.rs
@@ -1,74 +1,87 @@
-use core::slice;
+use core::{ops::RangeBounds, slice};
 use std::alloc;
 
-/// A memory buffer allocated on the heap, where the start position and end position are both
-/// aligned to `align` bytes. This is useful for working with O_DIRECT file IO, where the
-/// filesystem will often expect the buffer to be aligned to the logical block size (typically 512
-/// bytes).
+/// A memory buffer allocated on the heap, where the start position and end position of the backing
+/// buffer are both aligned. This is useful for working with O_DIRECT file IO, where the filesystem
+/// will often expect the buffer to be aligned to the logical block size (typically 512 bytes).
 #[derive(Debug)]
-pub struct AlignedBuffer {
+pub struct AlignedBuffer<R>
+where
+    R: RangeBounds<usize> + ExactSizeIterator,
+{
     buf: *mut u8,
-    len: usize,          // The number of bytes requested by the user.
-    start_offset: usize, // The number of bytes unused at the start of the buffer.
-    layout: alloc::Layout, // `layout.size()` gives the number of bytes _actually_ allocated,
-                         // which will be a multiple of `align`.
+    /// The slice requested by the user.
+    valid_slice: R,
+    /// `layout.size()` gives the number of bytes _actually_ allocated, which will be
+    /// a multiple of `align`.
+    layout: alloc::Layout,
 }
 
-unsafe impl Send for AlignedBuffer {}
+unsafe impl<R> Send for AlignedBuffer<R> where R: RangeBounds<usize> + ExactSizeIterator {}
 
-impl AlignedBuffer {
+impl<R> AlignedBuffer<R>
+where
+    R: RangeBounds<usize> + ExactSizeIterator,
+{
     /// Aligns the start and end of the buffer with `align`.
     /// 'align' must not be zero, and must be a power of two.
-    pub(crate) fn new(len: usize, align: usize, start_offset: usize) -> Self {
-        assert_ne!(len, 0);
+    pub(crate) fn new(slice: R, align: usize) -> Self {
+        assert_ne!(slice.len(), 0);
         // Let's say the user requests a buffer of len 3 and offset 2; and align is 4:
         //            index:     0 1 2 3 4 5 6 7
         //   aligned blocks:     |------|------|
         //        requested:         |---|
         // In this case, we need to allocate 8 bytes becuase we need to move the start
         // backwards to the first byte, and move the end forwards to the eighth byte.
-        let layout = alloc::Layout::from_size_align(len + (start_offset % align), align)
-            .expect("failed to create Layout!")
-            .pad_to_align();
+        let layout =
+            alloc::Layout::from_size_align(slice.len() + (slice.start_bound() % align), align)
+                .expect("failed to create Layout!")
+                .pad_to_align();
         let buf = unsafe { alloc::alloc(layout) };
         if buf.is_null() {
             alloc::handle_alloc_error(layout);
         }
         Self {
             buf,
-            len,
-            start_offset,
+            valid_slice: slice,
             layout,
         }
     }
 
     pub(crate) fn aligned_start_offset(&self) -> usize {
-        (self.start_offset / self.layout.align()) * self.layout.align()
+        (self.valid_slice.start_bound() / self.layout.align()) * self.layout.align()
     }
 
-    pub(crate) const fn len(&self) -> usize {
-        self.len
+    /// The length of the `valid_slice` requested by the user. The `valid_slice` is a view into the
+    /// underlying buffer. The underlying buffer may be larger than `len`.
+    pub(crate) fn len(&self) -> usize {
+        self.valid_slice.len()
     }
 
-    pub(crate) const fn aligned_len(&self) -> usize {
+    /// The length of the underlying buffer.
+    pub(crate) const fn capacity(&self) -> usize {
         self.layout.size()
     }
 
-    pub(crate) fn as_ptr(&mut self) -> *mut u8 {
+    pub(crate) fn as_ptr_to_underlying_buf(&mut self) -> *mut u8 {
         self.buf
     }
 
     pub fn as_slice(&self) -> &[u8] {
         unsafe {
             slice::from_raw_parts(
-                self.buf.offset(self.start_offset.try_into().unwrap()),
-                self.len,
+                self.buf
+                    .offset(self.valid_slice.start_bound().try_into().unwrap()),
+                self.len(),
             )
         }
     }
 }
 
-impl Drop for AlignedBuffer {
+impl<R> Drop for AlignedBuffer<R>
+where
+    R: RangeBounds<usize> + ExactSizeIterator,
+{
     fn drop(&mut self) {
         unsafe { alloc::dealloc(self.buf, self.layout) };
     }
@@ -82,8 +95,8 @@ mod tests {
     fn test_write_and_read() {
         // Create a new buffer:
         const LEN: usize = 16;
-        let mut aligned_buf1 = AlignedBuffer::new(LEN, 8, 0);
-        let mut aligned_buf2 = AlignedBuffer::new(LEN, 8, 0);
+        let mut aligned_buf1 = AlignedBuffer::new(..LEN, 8);
+        let mut aligned_buf2 = AlignedBuffer::new(..LEN, 8);
 
         // Set the values of the buffer:
         {
