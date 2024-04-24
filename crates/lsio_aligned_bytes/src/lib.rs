6e9fd2364b9a301a51f45e8f3e3cb5c13854d1d5
Jack Kelly <jack@openclimatefix.org>
2024-04-24 15:44:06 +0100

    Tidy up docstring

--- crates/lsio_aligned_bytes/src/lib.rs
+++ crates/lsio_aligned_bytes/src/lib.rs
@@ -35,8 +35,9 @@
 //!
 //! Unlike `bytes`, `aligned_bytes` does not use a `vtable`, nor does it allow users to grow the
 //! backing buffers. `aligned_bytes` implements the minimal set of features required for the rest
-//! of the LSIO project! In fact, the only way to write data into an `AlignedBytesMut` is via
-//! [`AlignedBytesMut::as_mut_ptr`] (because that's what the operating system expects!)
+//! of the LSIO project! In fact, `aligned_bytes` is _so_ minimal that the only way to write data
+//! into an `AlignedBytesMut` is via [`AlignedBytesMut::as_mut_ptr`] (because that's what the
+//! operating system expects!)
 //!
 //! # Examples and use-cases
 //!
