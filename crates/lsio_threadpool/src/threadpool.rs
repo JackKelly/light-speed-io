4258c29426080c4d1b578677a50a5ff9e8385d03
Jack Kelly <jack@openclimatefix.org>
2024-05-10 16:50:07 +0100

    ThreadPool works well!

--- crates/lsio_threadpool/src/threadpool.rs
+++ crates/lsio_threadpool/src/threadpool.rs
@@ -1,10 +1,138 @@
-use std::thread::{self, ThreadId};
+use std::{
+    collections::VecDeque,
+    sync::{
+        atomic::{self, AtomicBool},
+        mpsc,
+    },
+    thread::{self, Thread},
+    time::Duration,
+};
+
+use crossbeam::deque;
+
+use crate::worker::WorkStealer;
 
 pub(crate) enum ThreadPoolCommand {
     WakeAtMostNThreads(u32),
-    ThreadIsParked(ThreadId),
+    ThreadIsParked(Thread),
 }
 
-pub struct ThreadPool {
-    thread_handles: Vec<thread::JoinHandle<()>>,
+fn threadpool<T, I>(
+    n_threads: usize,
+    injector: &deque::Injector<I>,
+    keep_running: &AtomicBool,
+    task: T,
+) where
+    T: FnMut(WorkStealer<I>, &AtomicBool, mpsc::Sender<ThreadPoolCommand>) + Send + Clone,
+    I: Send,
+{
+    let (tx, rx) = mpsc::channel::<ThreadPoolCommand>();
+
+    // Create work stealing queues:
+    let mut local_queues: Vec<Option<deque::Worker<I>>> = (0..n_threads)
+        .map(|_| Some(deque::Worker::new_fifo()))
+        .collect();
+    let stealers: Vec<_> = local_queues
+        .iter()
+        .map(|local_queue| local_queue.as_ref().unwrap().stealer())
+        .collect();
+
+    thread::scope(|s| {
+        // Manager thread:
+        s.spawn(move || {
+            let mut parked_threads = VecDeque::<Thread>::with_capacity(n_threads);
+            while keep_running.load(atomic::Ordering::Relaxed) {
+                match rx.recv_timeout(Duration::from_millis(100)) {
+                    Ok(command) => match command {
+                        ThreadPoolCommand::ThreadIsParked(thread) => {
+                            parked_threads.push_back(thread);
+                        }
+                        ThreadPoolCommand::WakeAtMostNThreads(n) => {
+                            for _ in 0..n {
+                                match parked_threads.pop_front() {
+                                    Some(thread) => thread.unpark(),
+                                    None => break,
+                                }
+                            }
+                        }
+                    },
+                    Err(mpsc::RecvTimeoutError::Disconnected) => break,
+                    Err(mpsc::RecvTimeoutError::Timeout) => (),
+                }
+            }
+        });
+
+        // Worker threads:
+        for i in 0..n_threads {
+            let work_stealer =
+                WorkStealer::new(injector, local_queues[i].take().unwrap(), &stealers);
+
+            let tx_clone = tx.clone();
+            let mut task_clone = task.clone();
+            s.spawn(move || (task_clone)(work_stealer, keep_running, tx_clone));
+        }
+
+        drop(tx);
+    });
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_threadpool() {
+        const N_THREADS: usize = 4;
+        const N_TASKS: usize = 32;
+        let keep_running = AtomicBool::new(true);
+        let injector = deque::Injector::<usize>::new();
+
+        thread::scope(|s| {
+            let (output_tx, output_rx) = mpsc::channel::<usize>();
+
+            // Start threadpool of workers:
+            s.spawn(|| {
+                threadpool(
+                    N_THREADS,
+                    &injector,
+                    &keep_running,
+                    move |mut work_stealer: WorkStealer<usize>,
+                          keep_running: &AtomicBool,
+                          _tx: mpsc::Sender<ThreadPoolCommand>| {
+                        while keep_running.load(atomic::Ordering::Relaxed) {
+                            match work_stealer.find_task() {
+                                Some(task) => {
+                                    println!("thread: {:?}; task:{task}", thread::current().id());
+                                    output_tx.send(task).unwrap();
+                                }
+                                None => continue,
+                            };
+                        }
+                    },
+                );
+            });
+
+            // Wait a moment for all the threads to "come up":
+            thread::sleep(Duration::from_millis(10));
+
+            // Push tasks onto the global injector queue:
+            for i in 0..N_TASKS {
+                injector.push(i);
+            }
+
+            // Collect outputs and stop the work when all the outputs arrive:
+            let keep_running_ref = &keep_running;
+            s.spawn(move || {
+                let mut outputs = Vec::with_capacity(N_TASKS);
+                for _ in 0..N_TASKS {
+                    outputs.push(output_rx.recv().unwrap());
+                }
+                keep_running_ref.store(false, atomic::Ordering::Relaxed);
+                outputs.sort();
+                assert!(outputs.into_iter().eq(0..N_TASKS));
+                assert!(output_rx.try_recv().is_err());
+            });
+        });
+    }
 }
--- crates/lsio_threadpool/src/worker.rs
+++ crates/lsio_threadpool/src/worker.rs
@@ -1,33 +1,14 @@
-use core::sync;
-use std::{
-    iter,
-    sync::{
-        atomic::{self, AtomicBool},
-        mpsc,
-    },
-    thread,
-};
+use std::iter;
 
 use crossbeam::deque;
 
-use crate::threadpool::ThreadPoolCommand::{self, *};
-
 /// This object doesn't implement the actual worker loop.
-/// Instead, a `WorkStealer` instance should be part of a user-defined
-/// object. And that object should implement the worker loop, and can call the
-/// helper methods in the `WorkStealer`.
 pub struct WorkStealer<'a, T>
 where
     T: Send,
 {
-    /// Shared across threads. Set to `true` to stop threads gracefully.
-    stop: &'a AtomicBool,
-
-    /// Send instructions to the `ThreadPool`.
-    channel_to_threadpool: mpsc::Sender<ThreadPoolCommand>,
-
     /// Queues for implementing work-stealing:
-    global_queue: &'a deque::Injector<T>,
+    injector: &'a deque::Injector<T>,
     local_queue: deque::Worker<T>,
     stealers: &'a [deque::Stealer<T>],
 }
@@ -37,39 +18,15 @@ where
     T: Send,
 {
     pub fn new(
-        stop: &'a AtomicBool,
-        channel_to_threadpool: mpsc::Sender<ThreadPoolCommand>,
-        global_queue: &'a deque::Injector<T>,
+        injector: &'a deque::Injector<T>,
         local_queue: deque::Worker<T>,
         stealers: &'a [deque::Stealer<T>],
     ) -> Self {
         Self {
-            stop,
-            channel_to_threadpool,
-            global_queue,
+            injector,
             local_queue,
             stealers,
         }
-    }
-
-    pub fn ask_to_wake_other_threads(&self) {
-        let n = self.local_queue.len();
-        if n > 1 {
-            self.channel_to_threadpool
-                .send(WakeAtMostNThreads(n as _))
-                .unwrap();
-        }
-    }
-
-    pub fn stop(&self) -> bool {
-        self.stop.load(atomic::Ordering::Relaxed)
-    }
-
-    pub fn park(&self) {
-        self.channel_to_threadpool
-            .send(ThreadIsParked(thread::current().id()))
-            .unwrap();
-        thread::park();
     }
 
     pub fn find_task(&mut self) -> Option<T> {
@@ -80,7 +37,7 @@ where
             // Otherwise, we need to look for a task elsewhere.
             iter::repeat_with(|| {
                 // Try stealing a batch of tasks from the global queue.
-                self.global_queue
+                self.injector
                     .steal_batch_and_pop(&self.local_queue)
                     // Or try stealing a task from one of the other threads.
                     .or_else(|| self.stealers.iter().map(|s| s.steal()).collect())
@@ -90,79 +47,5 @@ where
             // Extract the stolen task, if there is one.
             .and_then(|s| s.success())
         })
-    }
-}
-
-#[cfg(test)]
-
-mod tests {
-    use std::time::Duration;
-
-    use super::*;
-
-    #[test]
-    fn test_work_stealer() {
-        let stop = AtomicBool::new(false);
-        let (channel_to_threadpool, _threadpool_rx) = mpsc::channel();
-        const N_THREADS: usize = 4;
-        const N_TASKS: usize = N_THREADS * 10;
-
-        // Create work stealing queues:
-        let global_queue: deque::Injector<usize> = deque::Injector::new();
-        let mut local_queues: Vec<Option<deque::Worker<usize>>> = (0..N_THREADS)
-            .map(|_| Some(deque::Worker::new_fifo()))
-            .collect();
-        let stealers: Vec<_> = local_queues
-            .iter()
-            .map(|local_queue| local_queue.as_ref().unwrap().stealer())
-            .collect();
-
-        thread::scope(|s| {
-            let (output_tx, output_rx) = mpsc::channel();
-
-            // Spawn N_THREADS threads
-            for i in 0..N_THREADS {
-                let mut work_stealer = WorkStealer::new(
-                    &stop,
-                    channel_to_threadpool.clone(),
-                    &global_queue,
-                    local_queues[i].take().unwrap(),
-                    &stealers,
-                );
-
-                let my_output_tx = output_tx.clone();
-                s.spawn(move || {
-                    while !work_stealer.stop() {
-                        match work_stealer.find_task() {
-                            Some(task) => {
-                                println!("thread: {:?}; task:{task}", thread::current().id());
-                                my_output_tx.send(task).unwrap();
-                            }
-                            None => continue,
-                        };
-                    }
-                });
-            }
-
-            drop(output_tx);
-
-            // Push "tasks" onto the global queue:
-            for i in 0..N_TASKS {
-                global_queue.push(i);
-            }
-
-            // Kill all the worker threads after a little while:
-            let stop_ref = &stop;
-            s.spawn(move || {
-                let mut outputs = Vec::with_capacity(N_TASKS);
-                for _ in 0..N_TASKS {
-                    outputs.push(output_rx.recv().unwrap());
-                }
-                stop_ref.store(true, atomic::Ordering::Relaxed);
-                outputs.sort();
-                assert!(outputs.into_iter().eq(0..N_TASKS));
-                assert!(output_rx.try_recv().is_err());
-            });
-        });
     }
 }
