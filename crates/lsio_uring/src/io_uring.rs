29bf625a81b57d3e8e26055f08efb3f93f5e49e0
Jack Kelly <jack@openclimatefix.org>
2024-05-17 10:29:17 +0100

    move IoUring to io_uring.rs

---
+++ crates/lsio_uring/src/io_uring.rs
@@ -0,0 +1,43 @@
+use std::{ffi::CString, os::unix::ffi::OsStrExt};
+
+use crate::get_ranges::GetRanges;
+use crate::operation::Operation;
+use crate::worker::UringWorker;
+use lsio_io::{Output, Reader};
+use lsio_threadpool::{ThreadPool, WorkerThread};
+
+pub struct IoUring {
+    threadpool: ThreadPool<Operation>,
+    output_rx: crossbeam_channel::Receiver<anyhow::Result<Output>>,
+}
+
+impl IoUring {
+    pub fn new(n_worker_threads: usize) -> Self {
+        let (output_tx, output_rx) = crossbeam_channel::bounded(1_024);
+        Self {
+            threadpool: ThreadPool::new(
+                n_worker_threads,
+                move |worker_thread: WorkerThread<Operation>| {
+                    let mut uring_worker = UringWorker::new(worker_thread, output_tx.clone());
+                    uring_worker.run();
+                },
+            ),
+            output_rx,
+        }
+    }
+}
+
+impl Reader for IoUring {
+    fn get_ranges(
+        &mut self,
+        location: &std::path::Path,
+        ranges: Vec<std::ops::Range<isize>>,
+        user_data: Vec<u64>,
+    ) -> anyhow::Result<()> {
+        let location = CString::new(location.as_os_str().as_bytes())
+            .expect("Failed to convert path '{path}' to CString");
+        let task = Operation::GetRanges(GetRanges::new(location, ranges, user_data));
+        self.threadpool.push(task);
+        Ok(())
+    }
+}
